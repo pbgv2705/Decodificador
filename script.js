4bae8f8dfe9da0f449611900ccbf2eee9d595359
Paula Gimenez <paulaherbal@gmail.com>
2023-02-27 18:50:43 -0300

    Melhorar a funcionalidade do Js, a apariencia do CSS, agregar botão copiar

--- script.js
+++ script.js
@@ -2,61 +2,29 @@
 var inputText = document.getElementById("entrada");
 var outputText = document.querySelector("textoSaida");
 const btncriptografar = document.querySelector("button");
-var copyText = " ";
 
-
-var codigos = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-console.log(codigos);
-
-
-
-function callCriptografar() {
-    console.log("inputText.value = " + inputText.value)
-    let msgCriptografado = fncriptografar(inputText.value);
-    outputText = msgCriptografado;
-}
-function fncriptografar(strEntrada) {
-    console.log("entró en la funcion criptografar");
-    let strSaida = "";
-    strEntrada = strEntrada.toLowerCase();
-    console.log(strEntrada, "strEntrada = " + strEntrada);
-    strSaida = strEntrada.replaceAll("a", "ai");
-    console.log(strSaida);
-    /*  strSaida = strSaida.replaceAll("e", "enter");
-      console.log(strSaida);
-      strSaida = strSaida.replaceAll("i", "imes");
-      console.log(strSaida);
-      strSaida = strSaida.replaceAll("o", "ober");
-      console.log(strSaida);
-      strSaida = strSaida.replaceAll("u", "ufat");
-      console.log(strSaida); */
-      
-    document.getElementById("saida").innerHTML = strSaida;
-    console.log("area saida: " + document.getElementById("saida").innerHTML);
+// Função para criptografar um texto
+function criptografar() {
+    let entrada = document.getElementById("entrada").value;
+    let saida = entrada.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+    document.getElementById("saida").value = saida;
+    document.getElementById("menino").style.display="none";
 }
 
-function callDescriptografar() {
-    console.log("inputText.value = " + inputText.value)
-    let msgCriptografado = fndescriptografar(inputText.value);
-    outputText = msgCriptografado;
-}
-function fndescriptografar(strEntrada) {
-    console.log("entró en la funcion desencriptografar");
-    let strSaida = "";
-    strEntrada = strEntrada.toLowerCase();
-    console.log(strEntrada, "strEntrada = " + strEntrada);
-    strSaida = strEntrada.replaceAll("ai", "a");
-    console.log(strSaida);
+// Função para descriptografar um texto
+function descriptografar() {
+    let entrada = document.getElementById("entrada").value;
+    let saida = entrada.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+    document.getElementById("saida").value = saida;
+    document.getElementById("menino").style.display="none";
 }
 
-function callCopiar() {
-    console.log("entró en callCopiar");
-    outputText = " ";
-    inputText = copyText;
-    console.log("outputText = " + outputText, "inputText = " + inputText);
+function copiarTexto() {
+    let saida = document.getElementById("saida");
+    saida.select();
+    saida.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    alert("Texto copiado para a área de transferência!");
+    
 }
-
-/*function fnCopiar(str) {
-   
-}*/
 
